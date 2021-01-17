28d14fec1263360dc9ea3cca72e5cb474c683437
Imran Baig <imran.baig@vipps.no>
2021-01-17 15:37:04 +0100

    Updated the example project

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,14 +9,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-
-        val view = findViewById<View>(R.id.textView1)
-
-        view.setOnClickListener {
-
-            Toast.makeText(this@MainActivity, R.string.my_message, Toast.LENGTH_SHORT).show()
-        }
+        setContentView(R.layout.layout_example_constraint)
     }
 }
